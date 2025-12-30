32838bac79225ecbb82e5370cf0a305877517a10
duongcokhanh <khanh@story.co>
2025-12-30 13:30:10 +0700

    set up CORS

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ from pathlib import Path
 
 from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 import demucs.separate
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Mount the separated folder as static files
 app.mount("/audio", StaticFiles(directory="separated/htdemucs"), name="audio")
